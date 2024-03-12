c50f3a91a862d3a2651052a10c1ccb0cdaa0a70e
MayankRaii <149515529+MayankRaii@users.noreply.github.com>
2024-03-13 01:16:45 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-sycopg2.connect(
-                # host="localhost", database="postgres", user="postgres", password="Mahakal@9770")
-                # cur=conn.cursor()
-                # cur.execute("insert into NEWS( url,text,estimated_time,title,genre,compound,publisher,count_word,count_sent, count_stp_word,upos)) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",(url,  text,estimated_time,title,genre, compound,publisher,count_word, count_sent,  count_stp_word, json.dumps(dict1)))
-                # conn.commit()
